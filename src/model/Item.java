1491f2276a6242d4317f91bd75e50180e4902c2a
NimeshDilhan <nimeshdilhan354@gmail.com>
2021-04-21 10:35:43 +0530

    Insert updated

--- src/model/Item.java
+++ src/model/Item.java
@@ -29,7 +29,7 @@ import java.sql.*;
 					return "Error while connecting to the database for inserting."; 
 				}
 				// create a prepared statement
-				 String query = "insert into items(`itemID`,`itemCode`,`itemName`,`itemPrice`,`itemDesc`) values(?, ?, ?, ?, ?)";
+				 String query = "insert into items('itemID','itemCode','itemName','itemPrice','itemDesc') values(?, ?, ?, ?, ?)";
 				 PreparedStatement preparedStmt = con.prepareStatement(query);
 				 // binding values
 				 preparedStmt.setInt(1, 0);
